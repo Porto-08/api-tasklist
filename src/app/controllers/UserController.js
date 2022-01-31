1090a13a6540e28598bbfe4a351d98e2cd98b347
Samuel Porto <Elaedmais_18>
2022-01-31 16:37:22 -0300

    regra de negócio de usuários

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,7 +2,41 @@ import User from '../models/User';
 
 class UserController {
     async store(req, res) {
-        return res.json({ message: 'Tudo certo' });
+        const regexEmail =
+            /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+        const { username, email, password_hash } = req.body;
+
+        if (!username || !email || !password_hash) {
+            return res.status(400).json({ error: 'Missing fields' });
+        }
+
+        if (!regexEmail.test(email)) {
+            return res.status(400).json({ error: 'Invalid email' });
+        }
+
+        const userExists = await User.findOne({ where: { email } });
+
+        if (userExists) {
+            return res.status(400).json({ error: 'User already exists' });
+        }
+
+        try {
+            const user = await User.create({
+                username,
+                email,
+                password_hash,
+            });
+
+            return res.json({
+                message: 'User created successfully',
+                id: user.id,
+                name: user.username,
+                email: user.email,
+            });
+        } catch (error) {
+            return res.status(400).json({ error: 'Error creating user' });
+        }
     }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-    const user = await User.create({
-        username: 'Samuel',
-        email: 'samuel@gmail.com',
-        password_hash: '123456',
-    });
-
-    res.json(user);
-});
+routes.post('/user/create', UserController.store);
 
 export default routes;
